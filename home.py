1d31f6321e707a502e8ac9b7c5bb828cad667603
ATIKRAM <75317555+ATIKRAM@users.noreply.github.com>
2022-10-13 10:55:08 +0530

    Update home.py

--- home.py
+++ home.py
@@ -2,7 +2,7 @@
 import streamlit as st
 
 def app():
-	st.header("Car Price Prediction App")
+	st.header("Specification Driven Predictive App")
 	st.text("""
             This web app allows a user to predict the prices of a car based on their 
             engine size, horse power, dimensions and the drive wheel type parameters.
